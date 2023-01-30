ad153b0b6261fe8173421f4910ac772ad11c5aeb
dejen-achenef <dejen.achenef-ug@aau.edu.et>
2023-01-30 03:05:40 +0300

    Merge pull request #14 from Imran-imtiaz48/patch-2

--- Source-Code/BluringImage/script.js
+++ Source-Code/BluringImage/script.js
@@ -1,27 +1,19 @@
-// Disable ESLint for the entire file
-/* eslint-disable */
-
-// Select DOM elements
+/* eslint-disable*/
 const loadingText = document.querySelector(".loading-text");
 const bg = document.querySelector(".bg");
-
 let load = 0;
 
-// Function to update the loading state and apply styles
 const blurring = () => {
-  load++;
+  load += 1;
   if (load > 99) {
-    clearInterval(intervalId);
+    clearInterval(int);
   }
   loadingText.innerText = `${load}%`;
   loadingText.style.opacity = scale(load, 0, 100, 1, 0);
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 };
 
-// Start the interval to run the blurring function every 20ms
-const intervalId = setInterval(blurring, 20);
-
-// Utility function to scale a number from one range to another
-const scale = (num, inMin, inMax, outMin, outMax) => {
-  return ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+const int = setInterval(blurring, 20);
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
