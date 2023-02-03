ab57ebaceff59f9b10fd28c2d2b1926b3991a34d
dejen-achenef <dejen.achenef-ug@aau.edu.et>
2023-02-04 00:01:01 +0300

    Merge pull request #13 from Imran-imtiaz48/patch-1

--- Source-Code/BluringImage/script.js
+++ Source-Code/BluringImage/script.js
@@ -1,24 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const loadingText = document.querySelector(".loading-text");
-    const bg = document.querySelector(".bg");
-    let load = 0;
-  
-  // Function to update the loading state and apply styles
-  const blurring = () => {
-    load++;
-    if (load > 99) {
-      clearInterval(intervalId);
-    }
-    loadingText.innerText = `${load}%`;
-    loadingText.style.opacity = scale(load, 0, 100, 1, 0);
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
-  };
+// Disable ESLint for the entire file
+/* eslint-disable */
 
-  // Start the interval to run the blurring function every 20ms
-  const intervalId = setInterval(blurring, 20);
+// Select DOM elements
+const loadingText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
 
-  // Utility function to scale a number from one range to another
-  const scale = (num, inMin, inMax, outMin, outMax) => {
-    return ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
-  };
-});
+let load = 0;
+
+// Function to update the loading state and apply styles
+const blurring = () => {
+  load++;
+  if (load > 99) {
+    clearInterval(intervalId);
+  }
+  loadingText.innerText = `${load}%`;
+  loadingText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+};
+
+// Start the interval to run the blurring function every 20ms
+const intervalId = setInterval(blurring, 20);
+
+// Utility function to scale a number from one range to another
+const scale = (num, inMin, inMax, outMin, outMax) => {
+  return ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+};
